4d7685e51032ed5bc8cd30c43a1a3e14dae79a51
Kiarra Julien <kiarrajulien@yahoo.com>
2017-12-30 16:27:03 -0500

    added comments to hangman game, so that others can understand what's been done.

---
+++ hangman.js
@@ -0,0 +1,29 @@
+
+// inWord Function - checks wheather the letter is in the word or not
+
+function inWord(letter, word) {
+	if (word.includes(letter)) {
+  return "This letter is in the word!";
+  } else {
+  return "This letter is not in the word, please try again.";
+  }
+}
+
+console.log(inWord("h", "harry")); // Yes, should be true.
+console.log(inWord("d", "Michael")); // No, should be false.
+
+  
+function timesInWord(letter, word) {
+  var re = new RegExp(letter,"gi");
+  var isMatch = word.match(re);
+  if (isMatch != null) {
+   return isMatch.length;
+   } else {
+   return "No Match, Try again!";
+  }
+}
+
+console.log(timesInWord("i", "Mississippi")); // Should be 4 times
+console.log(timesInWord("J", "Kiarra")); // Should be None.
+
+
